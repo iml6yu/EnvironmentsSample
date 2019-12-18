bd1c80b0ead8f2c8b3621fc14cb3c04b0f2fd4b5
iml6yu <iml6yu@gmail.com>
2019-12-18 09:48:20 +0800

    EnvironmentsSample

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace EnvironmentsSample.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string Evn = "";
+        public IndexModel()
+        {
+            Evn = Startup.Configuration["Evn"];
+        }
+        public void OnGet()
+        {
+
+        }
+    }
+}
